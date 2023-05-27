1990cdd9581061af4c33085b76132de08bdf4eee
EyalKeysar <eyal@some.com>
2023-05-27 20:08:25 +0300

    firebase hosting work for app

--- main.js
+++
@@ -1,8 +0,0 @@
-let toggleDiv = document.getElementById("AddContentDiv");
-let extraContent = document.getElementById("AddContentFormDiv");
-let toggleButton = document.getElementById("AddContentToggleButtonID");
-
-toggleButton.addEventListener("click", function() {
-  extraContent.classList.toggle("AddContentFormStyle");
-  toggleButton.textContent = toggleButton.textContent == "ADD CONTENT" ? "Fill the form." : "ADD CONTENT"; // toggle button text.
-});
