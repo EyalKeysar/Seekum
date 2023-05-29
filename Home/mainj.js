c8fff05661cfb573ccefdfeba904e9c40cf42cf9
eyal_keysar <keysareyal@gmail.com>
2023-05-29 09:59:30 +0300

    fbdb works

--- Home/mainj.js
+++ Home/mainj.js
@@ -1,13 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC0ZC_xEv-Zxd9LQPOiv720_oxi9j9qLzU",
   authDomain: "seekum-1ce54.firebaseapp.com",
@@ -18,9 +9,9 @@ const firebaseConfig = {
   appId: "1:990280632064:web:ba7929efd9354226bcd5e5",
   measurementId: "G-51TWQVEK1M"
 };
-
+firebase.initializeApp(firebaseConfig);
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+var database = firebase.database();
 
-db.database().ref('users/').set({a: 100});
+
+database.ref('users/').set({'':''});
